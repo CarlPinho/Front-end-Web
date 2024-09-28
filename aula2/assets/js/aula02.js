5bfd8d8fe470c3c9d2a977bf2489316cc4624ef5
Carlos Pinho <carlos.pinho.data@gmail.com>
2024-09-28 08:20:33 -0300

    aula02

---
+++ aula2/assets/js/aula02.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ *  Variavel: Espaço de momoria que irá 
+ *  armazenar um valor somente durante a execução de um script.
+ * 
+ */
+
+
+/*
+let -   
+Var - 
+const - 
+*/
+
+var numero = 1
+var numero02 = 10
+var numero03 = 20
+let nome = "carlao"
+let nome1 = "carlitos"
+const idade = 51
+
+
+
+
+document.getElementById("variavel-1").innerText = 
+numero02 + " x " + numero03 + " = " + (numero02 * numero03)
+
+document.getElementById("variavel-2").innerText =
+numero03 + " - " + numero + " = " + (numero - numero03)
+
+document.getElementById("variavel-3").innerText =
+numero03 + " / " + numero + " = " + (numero03 / numero)
+
+
+
---
+++ aula2/assets/js/test.js
@@ -0,0 +1,35 @@
+
+
+
+var nome1 = "Carlos"
+var nome2 = "Vanessa"
+var nome3 = "Jão"
+var nome4 = "Larissa"
+var nome5 = "Polyana"
+var nome6 = "Garro"
+
+
+var nome1 = "CORINTHIANS"
+var nome2 = "SÃO PAULO"
+var nome3 = "PALMEIRAS"
+var nome4 = "SANTOS"
+var nome5 = "FLAMEGO"
+var nome6 = "VASCO"
+
+
+
+document.getElementById("nome-1").innerText = nome1 
+document.getElementById("nome-2").innerText = nome2
+document.getElementById("nome-3").innerText = nome3
+document.getElementById("nome-4").innerText = nome4
+document.getElementById("nome-5").innerText = nome5
+document.getElementById("nome-6").innerText = nome6
+
+.
+
+document.getElementById("nomes-1").innerText = nome1 
+document.getElementById("nomes-2").innerText = nome2
+document.getElementById("nomes-3").innerText = nome3
+document.getElementById("nomes-4").innerText = nome4
+document.getElementById("nomes-5").innerText = nome5
+document.getElementById("nomes-6").innerText = nome6.
